78dad2990146356804b93824c7dcd94b03392569
code-withGV <gauravvashistha654@gmail.com>
2025-05-11 15:41:38 +0530

    prblems

--- BinarySearch_Practice/ceilingOfNumber.java
+++ BinarySearch_Practice/ceilingOfNumber.java
@@ -1,7 +1,41 @@
+package BinarySearch_Practice;
 
 public class ceilingOfNumber {
+
     public static void main(String[] args) {
-        
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        int ans = ceilingNumber(arr, target);
+        System.out.println(ans);
     }
-    
+
+    static int ceilingNumber(int[] arr, int target) {
+        int start = 0;
+        int end = 0;
+
+        boolean isAsc = arr[start] <= arr[end];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            }
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else if (target < arr[mid]) {
+                    start = mid + 1;
+                }
+            }
+        }
+        return arr[start];
+
+    }
+
 }
